ced0c7f78cec4f63266a4a5ba35c46bc6f140c33
Ezra Mitchell <ezra.mitchell@smoothstack.com>
2021-08-31 10:13:01 -0600

    added more update driver tests

--- src/pages/drivers/edit-driver-widget/EditDriverWidget.spec.js
+++ src/pages/drivers/edit-driver-widget/EditDriverWidget.spec.js
@@ -1,37 +1,107 @@
+import {
+  render,
+  waitFor,
+  screen,
+  fireEvent,
+  act,
+} from "@testing-library/react";
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import EditDriverWidget from "./EditDriverWidget";
-import { configure, render } from "enzyme";
-import { waitFor } from "@testing-library/react";
 
-describe("edit driver widget test", () => {
-  var adapter = new MockAdapter(axios);
+describe("EditDriverWidget test", () => {
+  const adapter = new MockAdapter(axios);
 
-  adapter.onGet("/accounts/driver/1").reply(200, {
-    id: 1,
-    email: "email@example.com",
-    firstName: "first",
-    lastName: "last",
-    car: "blue car",
-    DOB: "2000-07-07",
-    phone: "5555555555",
+  beforeEach(() => {
+    adapter.reset();
+    adapter.onGet("/accounts/driver/1").replyOnce(200, {
+      id: 1,
+      email: "email@example.com",
+      firstName: "first",
+      lastName: "last",
+      DOB: "2000-07-20",
+      password: "",
+      phone: "5555555555",
+      car: "big ass car",
+    });
   });
 
-  configure({ adapter: new Adapter() });
-
   it("gets driver", () => {
-    const httpSpy = jest.spyOn(axios, "get");
-    const wrapper = render(
-      <EditDriverWidget
-        id="1"
-        close={() => {
-          throw new Error("didn't open");
-        }}
-      />
-    );
+    render(<EditDriverWidget id={1} close={() => {}} />);
     return waitFor(() => {
-      expect(httpSpy).toHaveBeenCalled();
+      expect(screen.getByDisplayValue("email@example.com")).toBeInTheDocument();
+      expect(screen.getByDisplayValue("first")).toBeInTheDocument();
+      expect(screen.getByDisplayValue("last")).toBeInTheDocument();
+      expect(screen.getByDisplayValue("5555555555")).toBeInTheDocument();
     });
+  });
+
+  it("updates driver without password", async () => {
+    var httpSpy = false;
+    var request;
+    adapter.onPost("/accounts/driver").reply((req) => {
+      request = req;
+      httpSpy = true;
+      return [
+        200,
+        {
+          id: 1,
+          email: "email@example.com",
+          firstName: "first",
+          lastName: "last",
+          DOB: "2000-07-20",
+          password: "",
+          phone: "5555555555",
+          car: "big ass car",
+        },
+      ];
+    });
+    render(<EditDriverWidget id={1} close={() => {}} />);
+    let email = await screen.findByDisplayValue("email@example.com"); //form data is loaded
+    fireEvent.change(email, { target: { value: "newEmail@example.com" } });
+
+    let submitButton = screen.getByTestId("submit");
+    submitButton.click();
+    await screen.findByDisplayValue("email@example.com"); //wait for async methods to finish
+    expect(httpSpy).toBeTruthy();
+    let data = JSON.parse(request.data);
+    expect(data.email).toEqual("newEmail@example.com");
+    expect(request.params["update-password"]).toEqual(false);
+  });
+
+  it("updates driver with password", async () => {
+    var httpSpy = false;
+    var request;
+    adapter.onPost("/accounts/driver").reply((req) => {
+      request = req;
+      httpSpy = true;
+      return [
+        200,
+        {
+          id: 1,
+          email: "email@example.com",
+          firstName: "first",
+          lastName: "last",
+          DOB: "2000-07-20",
+          password: "",
+          phone: "5555555555",
+          car: "big ass car",
+        },
+      ];
+    });
+    render(<EditDriverWidget id={1} close={() => {}} />);
+    let email = await screen.findByDisplayValue("email@example.com"); //form data is loaded
+    fireEvent.change(email, { target: { value: "newEmail@example.com" } });
+    let password = await screen.findByLabelText("Password");
+    fireEvent.change(password, { target: { value: "newPassword" } });
+
+    let submitButton = screen.getByTestId("submit");
+    submitButton.click();
+    await screen.findByDisplayValue("email@example.com"); //wait for async methods to finish
+    expect(httpSpy).toBeTruthy();
+    let data = JSON.parse(request.data);
+    expect(data.email).toEqual("newEmail@example.com");
+    expect(data.password).toEqual("newPassword");
+    expect(request.params["update-password"]).toEqual(true);
   });
 });
