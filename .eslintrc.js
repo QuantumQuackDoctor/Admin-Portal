56edfd11de26c1540c8b9e36ef54b516cfde4801
Ezra Mitchell <ezra.mitchell@smoothstack.com>
2021-10-18 10:16:32 -0600

    added jest environment to .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,18 @@
+/* eslint-env node */
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
 };
